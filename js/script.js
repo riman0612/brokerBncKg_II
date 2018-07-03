e7433ca75e08a511d1c826cd5ee4d2dc152666be
SenDev <sen.dev@mail.ru>
2018-07-03 16:00:38 +0600

    firsbase files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBTRkVT7tbugUbV0PGumc4RdGKCZby_DNc",
+    authDomain: "tempproject-9e7ae.firebaseapp.com",
+    databaseURL: "https://tempproject-9e7ae.firebaseio.com",
+    projectId: "tempproject-9e7ae",
+    storageBucket: "tempproject-9e7ae.appspot.com",
+    messagingSenderId: "682367898884"
+  };
+  firebase.initializeApp(config);
+
+  var messagesRef = firebase.database().ref('messages');
 // $('#myModal').on('shown.bs.modal', function () {
 //   $('#myInput').trigger('focus')
 // })
@@ -79,33 +91,95 @@
 
 
 
+// было включено
+// document.querySelector('.firstPageCardsPanelContainer').onmouseover = (e) => {
 
-document.querySelector('.firstPageCardsPanelContainer').onmouseover = (e) => {
 
+    
 
-    // console.log(e.target.className);
+//     if (e.target.className == 'borderCard') {
+    
+//         e.target.parentNode.querySelector('.opacityLayerBackground').style.opacity = 1;
+        
+//     };
+    
 
-    if (e.target.className == 'borderCard') {
-    // console.log('cardsWrapper IN');
-        e.target.parentNode.querySelector('.opacityLayerBackground').style.opacity = 1;
-        // console.log("e.target.querySelector('.opacityLayerBackground').style.opacity = 1;");
-    };
-    // document.querySelector('.cardsWrapper').onmouseout = (e) => {
-    //     // console.log('cardsWrapper OUT');
-    //     e.target.querySelector('.opacityLayerBackground').style.opacity = 0;
-    // };
+// };
 
-};
-
-document.querySelector('.firstPageCardsPanelContainer').onmouseout = (e) => {
-    if (e.target.className == 'borderCard') {
-    // console.log('cardsWrapper IN');
-        e.target.parentNode.querySelector('.opacityLayerBackground').style.opacity = 0;
-        // console.log("e.target.querySelector('.opacityLayerBackground').style.opacity = 1;");
-    };
-};
+// document.querySelector('.firstPageCardsPanelContainer').onmouseout = (e) => {
+//     if (e.target.className == 'borderCard') {
+    
+//         e.target.parentNode.querySelector('.opacityLayerBackground').style.opacity = 0;
+        
+//     };
+// };
 
 $('.nav-tabs a').on('click', function (e) {
   e.preventDefault()
   $(this).tab('show')
 })
+
+
+
+// editor submit
+    
+
+
+    document.getElementById('editorData').onsubmit = (e) => {
+        e.preventDefault();
+        
+        var name = getInputValue('naming');
+        var price = getInputValue('price');
+        var rate = getInputValue('rate');
+        var recoupment = getInputValue('recoupment');
+        var profit = getInputValue('profit');
+        var description = getInputValue('description');
+        var city = getInputValue('city');
+        var region = getInputValue('region');
+        var section = getInputValue('section');
+        var middleMonthTurnovers = getInputValue('middleMonthTurnovers');
+        var middleMonthCosts = getInputValue('middleMonthCosts');
+        var amountOfWorkers = getInputValue('amountOfWorkers');
+        var salary = getInputValue('salary');
+        var area = getInputValue('area');
+        var rentPrice = getInputValue('rentPrice');
+        var businessAge = getInputValue('businessAge');
+        var organizationalAndLegalForm = getInputValue('organizationalAndLegalForm');
+
+        saveMessage(name, price, rate, recoupment, profit, description, city, region, section, middleMonthTurnovers, middleMonthCosts, amountOfWorkers, salary, area, rentPrice, businessAge, organizationalAndLegalForm);
+        // console.log(name, price, rate, recoupment, profit, description, city, region, section, middleMonthTurnovers, middleMonthCosts, amountOfWorkers, salary, area, rentPrice, businessAge, organizationalAndLegalForm);
+
+    }
+    
+    function getInputValue(id) {
+        return document.getElementById(id).value
+    }
+
+
+    function saveMessage(name, price, rate, recoupment, profit, description, city, region, section, middleMonthTurnovers, middleMonthCosts, amountOfWorkers, salary, area, rentPrice, businessAge, organizationalAndLegalForm) {
+        var newMessageRef = messagesRef.push();
+        newMessageRef.set({
+            name: name,
+            price: price,
+            price: price,
+            recoupment: recoupment,
+            profit: profit,
+            description: description,
+            city: city,
+            region: region,
+            section: section,
+            middleMonthTurnovers: middleMonthTurnovers,
+            middleMonthCosts: middleMonthCosts,
+            amountOfWorkers: amountOfWorkers,
+            salary: salary,
+            area: area,
+            rentPrice: rentPrice,
+            businessAge: businessAge,
+            organizationalAndLegalForm: organizationalAndLegalForm
+        })
+    };
+
+
+
+
+
